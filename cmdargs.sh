515cd707476af4137f13ef73cffb8a040cb65b7e
Dennis Simpson <comp2137-01@zonzorp.net>
2024-03-14 10:39:01 -0400

    Made cmdargs demo do fun stuff

--- cmdargs.sh
+++ cmdargs.sh
@@ -1,11 +1,51 @@
 #!/bin/bash
+#
+# this script is going to demonstrate working with the command line arguments or parameters
 
-echo "There are $# things on the command line"
-echo "The first thing is '$1'"
-echo "The second thing is '$2'"
+# Variables
+interfacenames=()
 
-shift
+# Functions
+function displayhelp {
+	echo $(basename "$0") [-h] interfacename ...
+	echo "   -h  display the command line help"
+}
+function echoerror {
+	echo "**** Error: " "$*" "****"
+	echo
+}
 
-echo "shifted"
+# get the interface to check from the command line
+while [ $# -gt 0 ]; do
+	case "$1" in
+		-h )
+			displayhelp
+			exit
+			;;
+		* )
+			interfacenames+=("$1")
+			;;
+	esac
+	shift
+done
 
-echo "Now the first thing is '$1', the second thing is '$2', and the number of things is $#"
+# check that we got at least one interface on the command line
+if [ ${#interfacenames[@]} -eq 0 ]; then
+	echoerror "You didn't specify any interface names"
+	displayhelp
+	exit 1
+fi
+
+# Loop through the interface name sin the array
+for interface in ${interfacenames[@]}; do
+	# Find the ip address for the interface if we have one
+	ip=$(ip a s $interface| grep -w inet|awk '{print $2}'| sed 's,/.*,,')
+	# TODO: Test if the interface name is valid
+	# Check if there is an address, put an informational message where the address would be in the output
+	if [ "$ip" = "" ]; then
+		ip="No address configured"
+	fi
+
+	# Produce the interface information
+	echo "$interface: $ip"
+done
